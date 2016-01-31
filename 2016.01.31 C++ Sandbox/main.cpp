64d3f1959527b0660b1c256c6f00941d1e5117c8
Haig Beylerian <haig@wavedna.com>
2016-01-31 16:08:19 -0500

    Initial Commit

---
+++ 2016.01.31 C++ Sandbox/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2016.01.31 C++ Sandbox
+//
+//  Created by Haig Beylerian on 2016-01-31.
+//  Copyright © 2016 Haig Beylerian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
